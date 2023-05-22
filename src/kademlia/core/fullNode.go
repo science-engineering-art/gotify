c64835153765b49f90a1457ea56808d578aac7e3
rolysr <rolsanchez@yandex.com>
2023-05-21 20:03:22 -0400

    Add basic Ping RPC method

--- src/kademlia/core/fullNode.go
+++ src/kademlia/core/fullNode.go
@@ -1,9 +1,23 @@
 package core
 
-import "github.com/science-engineering-art/spotify/src/kademlia/structs"
+import (
+	"fmt"
+
+	"github.com/science-engineering-art/spotify/src/kademlia/structs"
+)
 
 type FullNode struct {
 	structs.Node
-	RoutingTable map[string][]structs.Node
+	RoutingTable *structs.RoutingTable
 	// StorageTable Storage
+}
+
+// TODO: NewFullNode method for node initializing
+
+func (n *FullNode) Ping(sender structs.Node) structs.Node {
+	err := n.RoutingTable.AddNode(sender)
+	if err != nil {
+		fmt.Println("An error ocurred when executing Ping: ", err)
+	}
+	return n.Node
 }
