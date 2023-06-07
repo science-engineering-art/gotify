0704553c85878d3215957095432b23bec56c6583
rolysr <rolsanchez@yandex.com>
2023-06-07 12:36:41 -0400

    feat(api): Add SongsQuery DTO

---
+++ api/models/songs_query.go
@@ -0,0 +1,8 @@
+package models
+
+type SongsQuery struct {
+	Artist string `json:"artist,omitempty" bson:"artist,omitempty"`
+	Album  string `json:"album,omitempty" bson:"album,omitempty"`
+	Genre  string `json:"genre,omitempty" bson:"genre,omitempty"`
+	Title  string `json:"title,omitempty" bson:"title,omitempty"`
+}
